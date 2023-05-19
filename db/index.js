115b628ff5180951aee6ff0ca19ee9dba646f02d
ayo <ayopro89@gmail.com>
2023-05-19 11:45:23 +0100

    GENERAL -Project initialization -Express installed and running

---
+++ db/index.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config()
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DIALECT,
+  port: process.env.DB_PORT /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
+
+const connection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+}
+
+
+
+module.exports = {
+  sequelize,
+  connection
+}
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const express = require('express')
+const app = express()
+const { sequelize } = require("./db")
+require('dotenv').config()
+
+
+
+const start = (async function ()  {
+  try {
+    app.get('/', (req, res) => {res.send('Welcome to taskify API')})
+    await app.listen(process.env.PORT || 2222)
+    console.log(`APP connected on port ${process.env.PORT}`)
+  } catch (error) {
+    console.log(`Cannot connect at PORT ${process.env.PORT}`)
+  }
+})()
+
+
+
+
+
 console.log('hello World')
